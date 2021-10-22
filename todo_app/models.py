990aa0ca50c6b2d54d398f296207919a4b8a6136
Jumana Shaji <jahanashaji2020@gmail.com>
2021-10-22 12:46:40 +0530

    model error solved'

--- todo_app/models.py
+++ todo_app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    name=models.CharField(maxlength=100)
+    name=models.CharField(max_length=100)
     priority=models.IntegerField()
 
