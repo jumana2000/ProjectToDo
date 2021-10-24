0f0df24944449a02bbf8786950687133390556e3
Jumana Shaji <jahanashaji2020@gmail.com>
2021-10-24 18:40:06 +0530

    Deletion done

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.task_view,name='task_view'),
+    path('delete/<int:taskid>/',views.delete,name='delete')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . models import Task
 # Create your views here.
 
-def home(request):
-    return render(request,"home.html")
+def task_view(request):
+    obj1=Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        priority = request.POST.get('priority')
+        obj = Task(name=name, priority=priority)
+        obj.save()
+    return render(request,"task_view.html",{'obj1':obj1})
+
+def delete(request,taskid):
+    task=Task.objects.get(id=taskid)
+    if request.method=='POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html',{'task':task})
