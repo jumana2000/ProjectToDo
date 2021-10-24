00d54c73d2135c8588012cf9cc41800946f1f1ce
Jumana Shaji <jahanashaji2020@gmail.com>
2021-10-24 19:26:46 +0530

    Update button added

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . models import Task
+from . forms import Todoforms
 # Create your views here.
 
 def task_view(request):
@@ -18,3 +19,11 @@ def delete(request,taskid):
         task.delete()
         return redirect('/')
     return render(request,'delete.html',{'task':task})
+
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form =Todoforms(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'task':task,'form':form})
