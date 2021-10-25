a3ae3cc565ec279d267185d75283bfd8b3a59b0c
Jumana Shaji <jahanashaji2020@gmail.com>
2021-10-25 11:53:13 +0530

    Delete Generic View completed

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     path('update/<int:id>/',views.update,name='update'),
     path('cbvtask/',views.TaskListView.as_view(),name='cbvtask'),
     path('cbvdetail/<int:pk>/',views.TaskDetailView.as_view(),name='cbvdetail'),
-    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate')
+    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.TaskDeleteView.as_view(), name='cbvdelete')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -3,7 +3,7 @@ from . models import Task
 from . forms import Todoforms
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView,DeleteView
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -12,6 +12,7 @@ class TaskListView(ListView):
     model = Task
     template_name = 'task_view.html'
     context_object_name = 'obj1'
+
 
 class TaskDetailView(DetailView):
     model = Task
@@ -23,8 +24,17 @@ class TaskUpdateView(UpdateView):
     template_name='update.html'
     context_object_name='task'
     fields=('name','priority','date')
-    def get_success_url(self):
-        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+    success_url = reverse_lazy('cbvtask')
+    # def get_success_url(self):
+    #     return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('cbvtask')
+
+
 def task_view(request):
     obj1=Task.objects.all()
     if request.method == 'POST':
